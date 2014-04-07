44248529752113dae47ed20c6c89706b7e549818
yawol <yuronir@gmail.com>
2014-04-07 19:46:29 +0900

    P0407 edit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/2d/302900d7babb00131993a143ef389820
@@ -0,0 +1,26 @@
+package questionBank;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class P0403 {
+	public static void main(){
+		Scanner input = new Scanner(System.in);
+		HashMap data = new HashMap();
+		String[] temp;
+		
+		int i = input.nextInt();
+		for( ; i > 0 ; i--){
+			temp = input.next().split(" ");
+			data.put(i, temp);
+		}
+		
+		for(int j = 0; j < data.size(); j++){
+			temp = (String[]) data.get(j);
+			int a = Integer.parseInt(temp[0]);
+			int b = Integer.parseInt(temp[1]);
+			
+			//a^b의 규칙성 찾아서 마지막 숫자 찾기.
+		}
+	}
+}
--- questionBank/src/questionBank/Main.java
+++ questionBank/src/questionBank/Main.java
@@ -1,6 +1,5 @@
 package questionBank;
 
-import java.util.HashMap;
 import java.util.Scanner;
 
 /*
@@ -11,29 +10,26 @@ public class Main {
 	public static void main(String[] args){
 		int x1, x2, y1, y2, r1, r2;
 		double d;
-		HashMap<Integer, int[]> data = new HashMap<Integer, int[]>();
-		String[] temp;
-		int[] temp2 = new int[6];
+		int[][] temp;
 		Scanner input = new Scanner(System.in);
 		int count = input.nextInt();
-		int result = -2;
+		temp = new int[count][6];
 		
 		//각 회차의 정보를 저장
 		for(int i = 0 ; i < count ; i++){
-			temp = input.next().split(" ");
-			for(int j = 0; j < temp.length; j++){
-				temp2[j] = Integer.parseInt(temp[j]);
+			for(int j = 0; j < 6; j++){
+				temp[i][j] = input.nextInt();
 			}
-			data.put(i, temp2);
 		}
 		
 		for(int i = 0; i < count; i++){
-			temp2 = (int[]) data.get(i);
-			x1 = temp2[0]; y1 = temp2[1]; r1 = temp2[2];
-			x2 = temp2[3]; y2 = temp2[4]; r2 = temp2[5];
+			
+			int result = -2;
+			x1 = temp[i][0]; y1 = temp[i][1]; r1 = temp[i][2];
+			x2 = temp[i][3]; y2 = temp[i][4]; r2 = temp[i][5];
 			
 			d = Math.sqrt((Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2)));
-			
+
 			if(r1 < r2){
 				int ttemp = r1;
 				r1 = r2;
